18c4aafbf51367f846efd60ed51d0b728c1e8537
Edgar <max.edgar98@gmail.com>
2021-07-04 17:57:43 +0300

    Ready

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
-todos.find(function(element) {
-  if(element.completed === false){
-    let task = element.title;
-    let id = element.userId;
-    users.find(function(element){
-      if(element.id === id && element.company.bs.includes('web')){
-        console.log(`У ${element.name} не выполнена задача: ${task}`);
-      }
-    }) 
-  }
-})
+const foundUser = users.find((user) => user.company.bs.includes('web'));
+
+const foundUncompletedToDos = todos
+  .filter(todo => todo.userId === foundUser.id)
+  .filter(userTodo => !userTodo.completed);
+
+console.log(foundUncompletedToDos);
+
